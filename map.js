ba780a5ac42ae9cadf751d5f0020eb429fa24347
Henry Leon <henryatusabes@gmail.com>
2022-08-06 15:49:32 +0000

    Add the map method

--- map.js
+++ map.js
@@ -1,19 +1,32 @@
-  
+
 /**
  * To run this file in Gitpod, use the 
  * command node map.js in the terminal
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+}
 
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
-
+const simplified = nums.map(function (num) { return num * 2 });
+console.log(simplified);
 
 // Simplfied w/ map() + arrow function
+const arrow = nums.map(num => num * 2);
+console.log(arrow);
 
 
 // With objects:
@@ -22,18 +35,24 @@ const students = [
     id: 1,
     name: 'Mark',
     profession: 'Developer',
-    skill: 'JavaScript'
+    skill: 'JavaScript',
+    age: 32
   },
   {
     id: 2,
     name: 'Ariel',
     profession: 'Developer',
-    skill: 'HTML'
+    skill: 'HTML',
+    age: 26
   },
   {
     id: 3,
     name: 'Jason',
     profession: 'Designer',
-    skill: 'CSS'
+    skill: 'CSS',
+    age: 28
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id, student.age]);
+console.log(studentsWithIds);
